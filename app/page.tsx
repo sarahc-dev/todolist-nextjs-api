c15cb4424149e494fd3753e4d0aa8d964b324ac3
sarahc-dev <sarahclements@hotmail.co.uk>
2023-09-27 16:35:12 +0100

    add first passing e2e test

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
             <h1>TODO</h1>
+            <p>There&apos;s nothing to do.</p>
         </main>
     );
 }
